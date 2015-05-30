31666d7b97662a9391d13a60c4888cfc3e9c2fec
yague <yague@gmail.com>
2015-05-30 12:18:18 +0200

    Hemos añadido instancias de las clases Productos, Clientes, Vendedores y Tiendas en la clase Principal

--- Tienda_Enrique_005/src/tienda/Principal.java
+++ Tienda_Enrique_005/src/tienda/Principal.java
@@ -4,6 +4,18 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		Productos productoUno = new Productos(null, null, 0);
+		Productos productoDos = new Productos(null, null, 0);
+		Productos productoTres = new Productos(null, null, 0);
+		Clientes clienteUno = new Clientes(null, null, null);
+		Clientes clienteDos = new Clientes(null, null, null);
+		Clientes clienteTres = new Clientes(null, null, null);
+		Vendedores vendedorUno = new Vendedores(null, null, null);
+		Vendedores vendedorDos = new Vendedores(null, null, null);
+		Vendedores vendedorTres = new Vendedores(null, null, null);
+		Tiendas tiendaUno = new Tiendas(null, null, null);
+		Tiendas tiendaDos = new Tiendas(null, null, null);
+		Tiendas tiendaTres = new Tiendas(null, null, null);
 
 	}
 
